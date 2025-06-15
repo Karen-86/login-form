b23fd43d472c20172e60b6cd9d4feaaf0b3a724b
Karen <techshift86@gmail.com>
2025-06-15 20:00:41 +0400

    fjdk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import Image from "next/image";
-import "./login.css";
+import { Space_Grotesk, Krona_One } from 'next/font/google';
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+
+const kronaOne = Krona_One({
+  subsets: ['latin'],
+  weight: ['400'],
+});
 
 import logo from "./360-logo.jpg";
 import banner from "./banner.jpg";
 
 const Login = () => {
   return (
-    <div className="login-page min-h-[100vh]  flex items-center relative">
-      <div className=" hidden md:block layers absolute top-[50%] transform-[translateY(-50%)] left-0    w-full min-h-[650px]">
+    <div className={`login-page min-h-[100vh]  flex items-center relative bg-white ${spaceGrotesk.className} `}>
+      {/* <div className=" hidden md:block layers absolute top-[50%] transform-[translateY(-50%)] left-0    w-full min-h-[650px]">
        
         <div className="layer layer-2 bg-[#D9D3D0] absolute w-[400px] h-[300px] top-0 "></div>
         <div className="layer layer-3 bg-[#333333] absolute w-[600px] h-[450px] bottom-[20px] "></div>
@@ -16,20 +26,31 @@ const Login = () => {
           <Image src={banner} alt="banner" className="h-[470px] object-cover" />
         </div>
         <div className="layer layer-4 bg-[#9886F7] absolute w-[120px] h-[140px] bottom-0 "></div>
+      </div> */}
+
+      <div className=" hidden md:block layers absolute top-[50%] transform-[translateY(-50%)] left-0  pt-[40%] h-0  w-full  ">
+        <div className="layer layer-2 bg-[#D9D3D0] absolute  top-0 h-0 pt-[20%] w-[30%]"></div>
+        <div className="layer layer-3 bg-[#333333] absolute  bottom-[20px] h-0 pt-[30%] w-[41%]"></div>
+        <div className="layer-1 absolute left-[40px] top-[50%] transform-[translateY(-50%)] w-[73%] h-0 pt-[29%]">
+          <Image src={banner} alt="banner" className="absolute top-0 left-0 w-full h-full object-cover" />
+        </div>
+        <div className="layer layer-4 bg-[#9886F7] absolute  bottom-0  h-0 pt-[9%] w-[8%]"></div>
       </div>
 
-      <div className="form-wrapper w-full max-w-[1400px] mx-auto px-[20px]">
+      <div className="form-wrapper w-full  mx-auto px-[20px] md:px-[80px]">
         <form
           action=""
-          className="backdrop-blur-3xl  bg-white/60 border border-gray-200 md:border-none relative mx-auto md:mr-[60px]  max-w-[420px]  rounded-[10px] p-[40px] pb-[50px] shadow-[7px_7px_0px_#5435F5] "
+          className="backdrop-blur-3xl  bg-white/60 border border-gray-200  relative mx-auto md:mr-[60px]  max-w-[524px]  rounded-[10px] p-[43px] pb-[80px] shadow-[9px_9px_0px_#5435F5] "
         >
           <div className="hidden md:block absolute top-0 right-[0]  transform-[translate(50%,-20%)] shadow-[0px_2px_10px_rgba(0,0,0,0.1)]">
-            <Image src={logo} alt="Logo" className="max-w-[115px]" />
+            <Image src={logo} alt="Logo" className="max-w-[130px]" />
           </div>
-          <h1 className=" text-[35px] text-center mb-[50px] text-transparent bg-clip-text bg-[linear-gradient(to_left,_#000000_22%,_#292929_61%,_#666666_70%)]">Welcome</h1>
-          <h4 className="text-[18px] tracking-[0.18px] font-medium mb-[29px]">Login to your Account</h4>
+          <h1 className={`text-[42px] text-center mb-[66px] text-transparent bg-clip-text bg-[linear-gradient(to_left,_#000000_22%,_#292929_61%,_#666666_80%)] ${kronaOne.className}`}>
+            Welcome
+          </h1>
+          <h4 className="text-[24px] tracking-[0.18px] font-medium mb-[33px]">Login to your Account</h4>
 
-          <div className="field text-[14px] mb-5">
+          <div className="field text-[14px] mb-[25px]">
             <label htmlFor="login-username" className="mb-[10px] block">
               User Name
             </label>
@@ -37,11 +58,11 @@ const Login = () => {
               type="text"
               id="login-username"
               placeholder="Username@360.one"
-              className="bg-white px-3 py-2 block w-full border border-gray-300 rounded-[7px] text-[14px]"
+              className="bg-white px-3 py-[10px] block w-full border border-gray-300 rounded-[7px] text-[16px]"
             />
           </div>
 
-          <div className="field text-[14px]  mb-5">
+          <div className="field text-[14px]  mb-[25px]">
             <label htmlFor="login-username" className="mb-[10px] block">
               Password
             </label>
@@ -49,15 +70,15 @@ const Login = () => {
               type="text"
               id="login-username"
               placeholder="*********"
-              className="bg-white px-3 py-2 block w-full border border-gray-300 rounded-[7px] text-[14px]"
+              className="bg-white px-3 py-[10px] block w-full border border-gray-300 rounded-[7px] text-[16px]"
             />
           </div>
-          <button className="btn cursor-pointer  px-3 py-2 bg-[#2C2C2C] hover:bg-[#000]  text-white rounded-[7px] text-center block w-full   mb-5">
+          <button className="btn cursor-pointer text-[20px]  px-3 py-2 bg-[#2C2C2C] hover:bg-[#000]  text-white rounded-[7px] text-center block w-full   mb-[14px]">
             Login
           </button>
 
           <div className="form-footer text-center">
-            <a href="#/" className="hover:underline text-center ">
+            <a href="#/" className="hover:underline text-center text-[18px]">
               Forget Password?
             </a>
           </div>
